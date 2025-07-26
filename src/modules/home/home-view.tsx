dc2f56c4734a0ab1018739dbef0df275bd0673c4
Rahul Raj <rrajchaudhary.hajipur@gmail.com>
2025-07-27 04:37:30 +0530

    agent schema and trpc routing

--- src/modules/home/home-view.tsx
+++ src/modules/home/home-view.tsx
@@ -1,17 +1,11 @@
 "use client";
 
-import { useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
-
-
 
 export default function Home() {
  
-  const trpc = useTRPC();
-  const {data } = useQuery(trpc.hello.queryOptions({text: "Desk AI World"}));
   return (
     <div className="flex flex-col p-4">
-      {data?.greeting}
+      <h1>Home</h1>
     </div>
   )
 }
